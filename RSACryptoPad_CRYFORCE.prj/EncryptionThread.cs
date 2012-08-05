e0d87d558402aab9d592b8f75741d53b4223cf39
draf <artem.drobanov@gmail.com>
2012-08-05 17:21:09 +0400

    Черновая правка блокнота с шифрованием. Реализовано выравнивание по границе 4-х байт для результата, подлежащего преобразованию в Base-64, что гарантирует корректное разделение строк декодером.

--- RSACryptoPad_CRYFORCE.prj/EncryptionThread.cs
+++ RSACryptoPad_CRYFORCE.prj/EncryptionThread.cs
@@ -40,6 +40,7 @@ namespace RSACryptoPad
 		/// <summary>
 		/// Сжатие данных, находящихся во входном потоке
 		/// </summary>
+		/// FBICRY
 		/// <param name="stream">Поток с данными для сжатия</param>
 		/// <returns>Поток со сжатыми данными</returns>
 		public Stream Compress(Stream stream)
@@ -62,6 +63,7 @@ namespace RSACryptoPad
 		/// <summary>
 		/// Получение данных из сжатого состояния
 		/// </summary>
+		/// FBICRY
 		/// <param name="stream">Поток со сжатыми данными</param>
 		/// <returns>Поток с исходными данными</returns>
 		public Stream Uncompress(Stream stream)
@@ -78,6 +80,44 @@ namespace RSACryptoPad
 			return new MemoryStream(output);
 		}
 
+		/// <summary>
+		/// Выравнивание потока по указанной границе
+		/// </summary>
+		/// <param name="input">Входной поток</param>
+		/// <param name="align">Выравнивание</param>
+		/// <returns>Выровненный поток</returns>
+		public Stream Align(Stream input, int align)
+		{
+			MemoryStream output = new MemoryStream();
+			input.Seek(0, SeekOrigin.Begin);
+			input.CopyTo(output);
+			output.SetLength(output.Length + (align - output.Length % align));
+			output.Seek(0, SeekOrigin.End);
+			uint dataSize = (uint)input.Length;
+			BinaryWriter bw = new BinaryWriter(output);
+			bw.Write(dataSize);
+			bw.Flush();
+			return output;
+		}
+
+		/// <summary>
+		/// Извлечение выровненного потока
+		/// </summary>
+		/// <param name="input">Входной поток</param>
+		/// <returns>Выровненный поток</returns>
+		public Stream DeAlign(Stream input)
+		{
+			input.Position = input.Length - sizeof(uint);
+			BinaryReader br = new BinaryReader(input);
+			uint dataSize = br.ReadUInt32();
+			input.SetLength(dataSize);
+			MemoryStream output = new MemoryStream();
+			input.Seek(0, SeekOrigin.Begin);
+			input.CopyTo(output);
+			output.Flush();
+			return output;
+		}
+
 		public string EncryptString(string inputString, int dwKeySize, string xmlString)
 		{
 			RSACryptoServiceProvider rsaCryptoServiceProvider = new RSACryptoServiceProvider(dwKeySize);
@@ -85,7 +125,7 @@ namespace RSACryptoPad
 			int keySize = dwKeySize / 8;
 			byte[] bytesInput = Encoding.UTF8.GetBytes(inputString);
 
-			// Сжатие GZip
+			// Сжатие GZip...
 			MemoryStream input = new MemoryStream(bytesInput);
 			MemoryStream output = (MemoryStream)Compress(input);
 			output.Seek(0, SeekOrigin.Begin);
@@ -93,23 +133,25 @@ namespace RSACryptoPad
 			input.Close();
 			output.Close();
 
+			//...и выравнивание по границе 4 байта
+			MemoryStream ms = new MemoryStream(bytes);
+			MemoryStream output2 = (MemoryStream)Align(ms, 4);
+			output2.Seek(0, SeekOrigin.Begin);
+			var alignedBytes = output2.ToArray();
+			ms.Close();
+			output2.Close();
+
 			// The hash function in use by the .NET RSACryptoServiceProvider here is SHA1
 			// int maxLength = ( keySize ) - 2 - ( 2 * SHA1.Create().ComputeHash( rawBytes ).Length );
 			int maxLength = keySize - 42;
-			int dataLength = bytes.Length;
+			int dataLength = alignedBytes.Length;
 			int iterations = dataLength / maxLength;
 			StringBuilder stringBuilder = new StringBuilder();
 			for(int i = 0; i <= iterations; i++)
 			{
 				byte[] tempBytes = new byte[(dataLength - maxLength * i > maxLength) ? maxLength : dataLength - maxLength * i];
-				Buffer.BlockCopy(bytes, maxLength * i, tempBytes, 0, tempBytes.Length);
+				Buffer.BlockCopy(alignedBytes, maxLength * i, tempBytes, 0, tempBytes.Length);
 				byte[] encryptedBytes = rsaCryptoServiceProvider.Encrypt(tempBytes, true);
-				// Be aware the RSACryptoServiceProvider reverses the order of encrypted bytes after encryption and before decryption.
-				// If you do not require compatibility with Microsoft Cryptographic API (CAPI) and/or other vendors.
-				// Comment out the next line and the corresponding one in the DecryptString function.
-				Array.Reverse(encryptedBytes);
-				// Why convert to base 64?
-				// Because it is the largest power-of-two base printable using only ASCII characters
 				stringBuilder.Append(Convert.ToBase64String(encryptedBytes));
 			}
 			return stringBuilder.ToString();
@@ -125,23 +167,25 @@ namespace RSACryptoPad
 			for(int i = 0; i < iterations; i++)
 			{
 				byte[] encryptedBytes = Convert.FromBase64String(inputString.Substring(base64BlockSize * i, base64BlockSize));
-				// Be aware the RSACryptoServiceProvider reverses the order of encrypted bytes after encryption and before decryption.
-				// If you do not require compatibility with Microsoft Cryptographic API (CAPI) and/or other vendors.
-				// Comment out the next line and the corresponding one in the EncryptString function.
-				Array.Reverse(encryptedBytes);
 				arrayList.AddRange(rsaCryptoServiceProvider.Decrypt(encryptedBytes, true));
 			}
 
 			byte[] bytesInput = arrayList.ToArray(Type.GetType("System.Byte")) as byte[];
 
-			// Сжатие GZip
-			MemoryStream input = new MemoryStream(bytesInput);
+			MemoryStream ms = new MemoryStream(bytesInput);
+			MemoryStream output2 = (MemoryStream)DeAlign(ms);
+			output2.Seek(0, SeekOrigin.Begin);
+			var bytesInput2 = output2.ToArray();
+			ms.Close();
+			output2.Close();
+
+			// Декомпрессия GZip и снятие выравнивания потока по границе байт
+			MemoryStream input = new MemoryStream(bytesInput2);
 			MemoryStream output = (MemoryStream)Uncompress(input);
 			output.Seek(0, SeekOrigin.Begin);
 			byte[] bytes = output.ToArray();
 			input.Close();
 			output.Close();
-
 			return Encoding.UTF8.GetString(bytes);
 		}
 	}
